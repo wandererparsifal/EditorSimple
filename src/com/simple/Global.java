2c79571f8c4aede78f72d5a8f29ba79965b0798a
wandererparsifal <wandererparsifal@gmail.com>
2018-09-06 19:17:32 +0800

    html 路径配置

--- src/com/simple/Global.java
+++ src/com/simple/Global.java
@@ -8,8 +8,15 @@ public class Global {
 
     public static String UPLOAD_PATH;
 
+    public static String HTML_PATH;
+
     @Value("#{configProperties['upload.path']}")
     public void setUploadPath(String path) {
         Global.UPLOAD_PATH = path;
+    }
+
+    @Value("#{configProperties['upload.html']}")
+    public void setHtmlPath(String path) {
+        Global.HTML_PATH = path;
     }
 }
